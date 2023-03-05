11816c1fe0ddaaec129401ffec3974fd4ec7325c
Dante <68009431+IsD4n73@users.noreply.github.com>
2023-03-06 00:01:37 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'F1 Paddock.',
+      title: 'F1 Paddock',
       theme: ThemeData(
 
         primarySwatch: Colors.red,
