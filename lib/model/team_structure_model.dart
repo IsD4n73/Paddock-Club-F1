306acfcff5594e20ccc0f8a1d6d026151f0b9d64
Dante <68009431+IsD4n73@users.noreply.github.com>
2023-03-19 00:20:35 +0100

    Create team_structure_model.dart

---
+++ lib/model/team_structure_model.dart
@@ -0,0 +1,15 @@
+class TeamStructure{
+    String teamName;
+    String piloteOne;
+    String piloteTwo;
+    String teamLogo;
+    String teamId;
+    
+    TeamStructure({
+        required this.teamName, 
+        required this.piloteOne,
+        required this.piloteTwo,
+        required this.teamLogo,
+        required this.teamId,
+    });
+}  
