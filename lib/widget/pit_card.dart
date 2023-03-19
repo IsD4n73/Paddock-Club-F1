fca9a0af333ce0cd4a748e9c95629e5afd5b7415
Dante <68009431+IsD4n73@users.noreply.github.com>
2023-03-19 20:15:58 +0100

    Create pit_card.dart

---
+++ lib/widget/pit_card.dart
@@ -0,0 +1,11 @@
+import "package:flutter/material.dart";
+import "package:paddock_club/common/colors.dart";
+
+class PitCard extends StatelessWidget {
+  const PitCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card();
+  }
+}
