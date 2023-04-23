80873af4e0bfc49f6006b88b559a9c63a2ee0daa
Dante <d.dante03@gmail.com>
2023-04-23 13:06:11 +0200

    result shimmer

--- lib/pages/qualy_result.dart
+++ lib/pages/qualy_result.dart
@@ -3,6 +3,7 @@
 import "package:flutter/material.dart";
 import "package:paddock_club/common/colors.dart";
 import "package:paddock_club/controller/result_api.dart";
+import "package:paddock_club/widget/placeholders/list_tiles.dart";
 import "package:paddock_club/widget/qualy_tile.dart";
 
 import "../model/result_model.dart";
@@ -16,16 +17,12 @@ class QualyResultPage extends StatefulWidget {
 }
 
 class _QualyResultPageState extends State<QualyResultPage> {
-  List<QualyRsultModel> results = [];
+  late Future<List<QualyRsultModel>> results;
 
   @override
   void initState() {
     super.initState();
-    getQualyRsult(widget.idRace).then((value) {
-      setState(() {
-        results = value;
-      });
-    });
+    results = getQualyRsult(widget.idRace);
   }
 
   @override
@@ -40,31 +37,39 @@ class _QualyResultPageState extends State<QualyResultPage> {
         ),
         centerTitle: true,
       ),
-      body: results.isNotEmpty
-          ? ListView.builder(
-            shrinkWrap: true,
-            physics: const BouncingScrollPhysics(),
-            itemCount: results.length,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: QualyTile(results[index]),
-              );
-            },
-          )
-          : const Center(
-              child: Padding(
-                padding: EdgeInsets.all(8),
-                child: Text(
-                  "Non sono presenti dati sulla sessione...",
-                  style: TextStyle(
-                    fontFamily: "F1Bold",
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-            ),
+      body: FutureBuilder(
+        future: results,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return snapshot.data!.isNotEmpty
+                ? ListView.builder(
+                    shrinkWrap: true,
+                    physics: const BouncingScrollPhysics(),
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: QualyTile(snapshot.data![index]),
+                      );
+                    },
+                  )
+                : const Center(
+                    child: Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Text(
+                        "Non sono presenti dati sulla sessione...",
+                        style: TextStyle(
+                          fontFamily: "F1Bold",
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  );
+          }
+          return const ListTilesShimmer();
+        },
+      ),
     );
   }
 }
--- lib/pages/race_result.dart
+++ lib/pages/race_result.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import "package:paddock_club/controller/result_api.dart";
 import "package:paddock_club/model/result_model.dart";
+import "package:paddock_club/widget/placeholders/list_tiles.dart";
 
 import "../common/colors.dart";
 import "../widget/standing_tile.dart";
@@ -15,17 +16,13 @@ class RaceRsultsPage extends StatefulWidget {
 }
 
 class _RaceRsultsPageState extends State<RaceRsultsPage> {
-  List<RaceRsultModel> results = [];
+  late Future<List<RaceRsultModel>> results;
 
   @override
   void initState() {
     super.initState();
 
-    getRaceResult(widget.idRace).then((value) {
-      setState(() {
-        results = value;
-      });
-    });
+    results = getRaceResult(widget.idRace);
   }
 
   @override
@@ -40,30 +37,38 @@ class _RaceRsultsPageState extends State<RaceRsultsPage> {
         ),
         centerTitle: true,
       ),
-      body: results.isNotEmpty
-          ? ListView.builder(
-              itemCount: results.length,
-              physics: const BouncingScrollPhysics(),
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: StandingTileShort(results[index], widget.idRace),
-                );
-              },
-            )
-          : const Center(
-              child: Padding(
-                padding: EdgeInsets.all(8),
-                child: Text(
-                  "Non sono presenti dati sulla sessione...",
-                  style: TextStyle(
-                    fontFamily: "F1Bold",
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-            ),
+      body: FutureBuilder(
+        future: results,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return snapshot.data!.isNotEmpty
+                ? ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    physics: const BouncingScrollPhysics(),
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: StandingTileShort(snapshot.data![index], widget.idRace),
+                      );
+                    },
+                  )
+                : const Center(
+                    child: Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Text(
+                        "Non sono presenti dati sulla sessione...",
+                        style: TextStyle(
+                          fontFamily: "F1Bold",
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  );
+          }
+          return const ListTilesShimmer();
+        },
+      ),
     );
   }
 }
